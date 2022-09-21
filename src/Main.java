f775b8990207cd1063a29d07d2cd058d8abbed2d
Kajstan <kajstankowski@gmail.com>
2022-09-21 20:43:38 +0200

    added substraction method

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Calculator cal1= new Calculator();
         System.out.println(cal1.addition(4,6));
+        System.out.println(cal1.substraction(8, 4));
     }
 }
